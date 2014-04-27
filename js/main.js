311aa7c6c674ca241ffb3a319115c25124c4db06
Jason Marmon <jtmarmon@gmail.com>
2014-04-27 17:42:55 -0400

    added standalones

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ function checkEmail(){
 	}
 }
 $(document).ready(function(){
-	
 	setTimeout(function(){
 		$('#f1_container').animate({'opacity':'1'},2000,'linear');
 		$('#tagline').animate({'opacity':'1'}, 2000,'linear');
@@ -29,12 +28,13 @@ $(document).ready(function(){
      var alpha = Math.min(0.4 * $(this).scrollTop() / 1, 0.9);
         $("#p1").css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
  $(document).scroll(function() {
-        var alpha = 0.6 * $(this).scrollTop() / 350;
+        var alpha = 0.6 * $(this).scrollTop() / 300;
 
 	    $("#p1").css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
 	    $("#info-wrapper").css('opacity', alpha);
 	    $('#tagline').css('opacity', 1-alpha);
 	    $("#introducing").css('opacity', 1-alpha);
+	    $("#f1_container").css('opacity', 1-alpha);
 	    if(alpha<=.2)
 	    {
 	    	$("#tagline").css({'opacity':'1','color':'white'});
@@ -48,11 +48,21 @@ $(document).ready(function(){
  		  if(percentageVisible>=1)
         	percentageVisible=1;
 	   // $("#info-wrapper").css('left',(100-(1/percentageVisible)*100) + "%");
-	    console.log((1/percentageVisible)*100 + 100);
  		 if(percentageVisible>=1)
  		 	percentageVisible=1;
  		 var offset = 100 - percentageVisible*100;
  		 $("#p2 *").css({'opacity':percentageVisible});
     });
- 
+	$("#tagline").waypoint(function(direction){
+		if(direction==="down")
+		{
+			$("#standalones").stop().animate({'left':'0'},1000, function(){
+				$("#standalones").effect('bounce',{direction:'left', times:'3', distance:'15', mode:'show'},600);
+			});
+		}
+		else
+		{
+			$("#standalones").stop().animate({'left':'-100%'},1000);
+		}
+	});
 });
