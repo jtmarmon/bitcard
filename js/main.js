340fbd212931f383301354656d51a16a6b2bc066
Jason Marmon <jtmarmon@gmail.com>
2014-04-27 10:34:16 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,45 @@
+
+function showPreorder(){
+	$("#i-want-it").addClass('i-want-clicked');
+	$("#waiting-for").hide();
+	setTimeout(function(){
+		$("#preorder-wrapper").show(500);
+	},750);
+}
+function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+} 
+function checkEmail(){
+	if(validateEmail(document.getElementById('emailInput').value))
+	{
+
+		$('.bitcard-button').show( 'slow');
+	}
+	else
+	{
+		$('.bitcard-button').hide('slow');
+	}
+}
+$(document).ready(function(){
+	
+	setTimeout(function(){
+		$('#f1_container').animate({'opacity':'1'},2000,'linear');
+		$('#tagline').animate({'opacity':'1'}, 2000,'linear');
+	},200);
+     var alpha = Math.min(0.4 * $(this).scrollTop() / 1, 0.9);
+        $("#p1").css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
+ $(document).scroll(function() {
+        var alpha = 0.6 * $(this).scrollTop() / 150;
+	    $("#p1").css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
+	    $("#p2").css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
+	    $('#p1 .theme-font').css('opacity', 1-alpha);
+
+ 		 var percentageVisible = $(window).scrollTop()/$(window).height()
+ 		 if(percentageVisible>=1)
+ 		 	percentageVisible=1;
+ 		 var offset = 100 - percentageVisible*100;
+ 		 $("#p2 *").css({'opacity':percentageVisible});
+    });
+ 
+});
